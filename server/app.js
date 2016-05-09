80ed4791978a37ae18d916f2ba2376320aaf4709
desak29 <desak29@icloud.com>
2016-05-09 12:14:53 -0400

    ~added new relic to app.js file

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var newrelic = require('newrelic');
+
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
